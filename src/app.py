63fc9a8bfcbd29e205b07a663791686790caf54f
Logan Chaves <loganchaves@icloud.com>
2019-04-11 11:09:09 -0400

    initial fix

---
+++ src/app.py
@@ -0,0 +1,31 @@
+
+from flask import Flask
+from .views.user_view import user_api
+from .models import db, bcrypt
+from .config import app_config
+from flask_cors import CORS
+
+from .views.user_view import user_api as user_blueprint
+from .views.blogpost_view import blogpost_api as blog_blueprint
+from .views.battle_view import battles_api as battles_blueprint
+
+def create_app(env_name):
+    '''
+    Create App
+    '''
+
+    # app initilaztion
+    app = Flask(__name__)
+
+    app.config.from_object(app_config[env_name])
+
+    CORS(app)
+
+    app.register_blueprint(user_api, url_prefix='/api/v1/users')
+    app.register_blueprint(blog_blueprint, url_prefix='/api/v1/blogpost')
+    app.register_blueprint(battles_blueprint, url_prefix='/api/v1/battles')
+
+    bcrypt.init_app(app)
+    db.init_app(app)
+
+    return app
---
+++ src/models/comments.py
@@ -0,0 +1,64 @@
+from . import db, bcrypt
+from datetime import datetime
+
+from marshmallow import fields, Schema
+
+
+
+class BlogPostModel(db.Model):
+    
+
+    __tablename__ = 'blogposts'
+
+    id = db.Column(db.Integer, primary_key=True)
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    created_at = db.Column(db.DateTime)
+    modified_at = db.Column(db.DateTime)
+
+    def __init__(self, data):
+     
+
+        self.owner_id = data.get('owner_id')
+        self.title = data.get('title')
+        self.content = data.get('content')
+        self.created_at = datetime.utcnow()
+        self.modified_at = datetime.utcnow()
+
+    def __repr__(self):
+        return f'<id {self.id}>'
+
+    def delete(self):
+    
+        db.session.delete(self)
+        db.session.commit()
+
+    def save(self):
+        
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self, data):
+        
+        for key, item in data.items():
+            setattr(self, key, item)
+        self.modified_at = datetime.utcnow()
+        db.session.commit()
+
+    @staticmethod
+    def get_all_blogposts():
+        return BlogPostModel.query.all()
+
+    @staticmethod
+    def get_one_blogpost(id):
+        return BlogPostModel.query.get(id)
+
+
+# models/blog_post.py
+class BlogPostSchema(Schema):
+    id = fields.Int(dump_only=True)
+    title = fields.Str(required=True)
+    content = fields.Str(required=True)
+    owner_id = fields.Int(required=True)
+    created_at = fields.DateTime(dump_only=True)
+    modified_at = fields.DateTime(dump_only=True)
