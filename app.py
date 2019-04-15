090254ef3e280ac283d79164f8e39c684ca38d19
xd <xmandunn@gmail.com>
2019-04-15 15:16:53 -0400

    moved to different app.py

--- app.py
+++ app.py
@@ -2,9 +2,11 @@ import os
 
 from src.app import create_app
 from flask_heroku import Heroku
+from flask_cors import CORS
 
 env_name = os.getenv('FLASK_ENV')
 app = create_app(env_name)
+CORS(app)
 heroku = Heroku(app)
 
 if __name__ == '__main__':
--- src/app.py
+++ src/app.py
@@ -2,7 +2,6 @@ from flask import Flask
 from .views.user_view import user_api
 from .models import db, bcrypt
 from .config import app_config
-from flask_cors import CORS
 from .views.profile_view import profile_api as profile_blueprint
 from .views.user_view import user_api as user_blueprint
 from .views.blogpost_view import comment_api as comment_blueprint
@@ -18,8 +17,6 @@ def create_app(env_name):
 
     app.config.from_object(app_config['development'])
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-    CORS(app)
 
     app.register_blueprint(user_api, url_prefix='/api/v1/users')
     app.register_blueprint(profile_blueprint, url_prefix='/api/v1/profiles')
